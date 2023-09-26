267e7ca78b6a760edb0fea2a4f016261dfc68056
Abhishek Bhatia <abhishekbhatia08@gmail.com>
2023-09-26 13:58:52 -0400

    Updated script to use only clean data

--- scripts/01_dataprocessing.R
+++ scripts/01_dataprocessing.R
@@ -2,92 +2,55 @@
 
 ## 1. Load required packages ----
 library(here)
-library(plyr)
-library(data.table)
 library(tidyverse)
-library(fs)
 library(janitor)
-library(raster)
-library(rgdal)
 library(sf)
 
-## 2. Declare `here`
+## 2. Set working directory ----
 here::i_am("scripts/01_dataprocessing.R")
 
 ## 3. Run Util.R ----
 source(here::here("scripts", "util.R"))
 
 ## 4. Create necessary directories ----
-ensure_directory(here::here("derived_data"))
+dir.create(here::here("derived_data"), showWarnings = FALSE)
 
 # 01 MERGING DATA ----
 
 ## 1. Load in all data ----
-
-### HVI data ----
 hvi_data <- read_csv(here::here("source_data", "heat_vulnerability", "hvi_data.csv")) %>% as.data.frame()
-
-### Census data ----
-true_census <- st_read(here::here("source_data", "spatial_tables", "true_cenusTracts.shp")) %>% as.data.frame()
-
-### Housing_heating data ----
-househeat <- read_csv(here::here("source_data", "housing_heating", "ACSST5Y2021.S2504-Data.csv")) %>% as.data.frame()
-
-### Housing_income data ----
-houseincome <- read_csv(here::here("source_data", "housing_income_costs", "ACSST5Y2021.S2503-Data.csv")) %>% as.data.frame()
-
-### Age race sex data ----
-agesexrace <- read_csv(here::here("source_data", "age_sex_race", "ACSDP5Y2021.DP05-Data.csv")) %>% as.data.frame()
+true_census <- st_read(here::here("source_data", "spatial_tables", "true_cenusTracts.shp"))
+househeat <- read_csv(here::here("source_data", "spatial_tables", "house_heating_final.csv")) %>% as.data.frame()
+houseincome <- read_csv(here::here("source_data", "spatial_tables", "house_income_final.csv")) %>% as.data.frame()
 
 ## 2. Merge datasets ----
 
-### Clean and keep only relevant GEOIDs ----
-process_and_filter_data <- function(df, hvi_data) {
-  # Split GEOID
-  df$GEOID <- as.character(str_extract(df$GEO_ID, "(?<=US)\\d+"))
-  
-  # Prepare for merging
-  names(df) <- df[1,]
-  df <- df[-1,]
-  names(df)[ncol(df)] <- "GEOID"
-  
-  # Filter rows based on GEOIDs in hvi_data
-  valid_geoids <- unique(hvi_data$GEOID)
-  df <- df[df$GEOID %in% valid_geoids, ]
-  df <- janitor::clean_names(df)
-  
-  return(df)
-}
-
-### Function to keep columns that start with "estimate_" and "GEOID"
-filter_estimate_columns <- function(df) {
-  cols_to_keep <- c("geoid", grep("^estimate_", names(df), value = TRUE))
-  return(df[, cols_to_keep])
-}
-
-
-### Process datasets ----
-agesexrace <- process_and_filter_data(agesexrace, hvi_data) %>% clean_names
-househeat <- process_and_filter_data(househeat, hvi_data) %>% clean_names
-houseincome <- process_and_filter_data(houseincome, hvi_data) %>% clean_names
-
-agesexrace <- filter_estimate_columns(agesexrace)
-househeat <- filter_estimate_columns(househeat)
-houseincome <- filter_estimate_columns(houseincome)
-
-### Merge datasets ----
+### Function to merge datasets ----
 merge_datasets <- function(main_df, df_to_merge) {
-  merged_df <- left_join(main_df, df_to_merge, by = "geoid")
+  merged_df <- left_join(main_df, df_to_merge, by = "GEOID")
   return(merged_df)
 }
 
-hvi_data$GEOID <- as.character(hvi_data$GEOID) 
-hvi_data <- janitor::clean_names(hvi_data)
-merged_df <- left_join(hvi_data, true_census, by = c("geoid" = "FIPS"))
-merged_df <- merge_datasets(merged_df, agesexrace)
-merged_df <- merge_datasets(merged_df, househeat)
-merged_df <- merge_datasets(merged_df, houseincome)
+hvi_data$GEOID <- as.character(hvi_data$GEOID)
+househeat$GEOID <- as.character(househeat$GEOID)
+houseincome$GEOID <- as.character(houseincome$GEOID)
 
-## Save dataframe in derived_data folder ----
+merged_df <- merge_datasets(hvi_data, househeat)
+merged_df <- merge_datasets(merged_df, houseincome)
+merged_df <- janitor::clean_names(merged_df)
+
+### Merge with true_census to create an sf object by geoid = FIPS ----
+merged_df <- left_join(true_census, merged_df, by = c("FIPS" = "geoid"))
+
+### Remove duplicate columns
+merged_df <- merged_df[, !duplicated(colnames(merged_df))]
+
+### Keep only rows where fips are in unique geoid in the HVI data
+merged_df <- merged_df[merged_df$FIPS %in% unique(hvi_data$GEOID), ]
+
+### Save dataframe in derived_data folder ----
 write_csv(merged_df, here::here("derived_data", "merged_data.csv"))
+
+
+
 
