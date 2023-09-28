628a9d1cf2bbf256e6dff8fa0f3a5f36573f17ce
Abhishek Bhatia <abhishekbhatia08@gmail.com>
2023-09-28 13:12:48 -0400

    Update 02_analysis.R

--- scripts/02_analysis.R
+++ scripts/02_analysis.R
@@ -5,6 +5,8 @@ library(here)
 library(tidyverse)
 library(janitor)
 library(sf)
+library(FactoMineR)
+
 
 ## 2. Set working directory ----
 here::i_am("scripts/02_analysis.R")
@@ -30,5 +32,59 @@ df <- read_csv(data_path) %>% as.data.frame()
 
 # # Subset for North Carolina
 # df <- df %>% filter(state == "Virginia")
+
+
+# 1. PCA on the dataframe
+# Exclude non-numeric columns and the "air temperature" column
+df_numeric <-
+  df %>% select(
+    -c(
+      geoid,
+      state,
+      name,
+      county,
+      night_mean_air_temperature,
+      morning_mean_air_temperature,
+      afternoon_mean_air_temperature
+    )
+  ) %>% select_if(is.numeric) 
+
+# Scale the data
+df_scaled <- scale(df_numeric)
+
+# Perform PCA
+pca_result <- PCA(df_scaled, graph = FALSE)
+
+# 2. Examine correlations between variables in each PC
+biplot(pca_result$eig, pca_result$var, cex = 0.7) 
+
+# 3. Correlation of each PC with "air temperature"
+# Extract the scores of the principal components
+pc_scores <- as.data.frame(pca_result$ind$coord)
+
+# Compute the correlation with "air temperature"
+correlations <-
+  sapply(pc_scores, function(pc)
+    cor(pc, df$afternoon_mean_air_temperature))
+
+# Print the correlations
+print(correlations)
+
+
+ # 1. Scree plot
+eig.val <- pca_result$eig
+
+eig.val<- eig.val %>%
+  as.data.frame() %>%
+  rownames_to_column("Principal Component") %>% janitor::clean_names() 
+eig.val$principal_component <- as.numeric(str_remove(eig.val$principal_component, "comp "))
+
+ggplot(data = eig.val, aes(x = principal_component, y = eigenvalue)) +
+  geom_line() +
+  geom_point(size = 4) +
+  ggtitle("Scree plot") +
+  xlab("Principal Component") +
+  ylab("Eigenvalue") +
+  theme_minimal()
 
 
