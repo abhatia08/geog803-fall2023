4121879cb4ae6c0587e555411a23693edac402c6
hgesandborn <hgesandborn@gmail.com>
2023-11-14 13:20:29 -0500

    Update 03_figures.R
    boxplots

--- scripts/03_figures.R
+++ scripts/03_figures.R
@@ -45,7 +45,18 @@ plot_temperature_distributions(tract_df, "disability", "no_disability")
 plot_temperature_distributions(tract_df, "disability", "no_disability")
 
 
+## Overall Boxplots
+names <- c("Minority population", "White population")
+
+boxplot(tract_df[,2:3], xlab = "All Tracts", ylab = "Night Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) 
+boxplot(tract_df[,4:5], xlab = "All Tracts", ylab = "Afternoon Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) 
+boxplot(tract_df[,6:7], xlab = "All Tracts", ylab = "Morning Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # morning
+
+
+
 ## split by climate zone
+
+plot_temperature_distributions(tract_df, "minority", "white")
 
 temperate <- filter(tract_df, climate_zone=="Temperate")
 arid <- filter(tract_df, climate_zone=="Arid")
@@ -56,6 +67,25 @@ plot_temperature_distributions(temperate, "minority", "white")
 plot_temperature_distributions(arid, "minority", "white")
 plot_temperature_distributions(snow, "minority", "white")
 plot_temperature_distributions(equatorial, "minority", "white")
+
+
+## Boxplots
+boxplot(temperate[,2:3], xlab = "Temperate", ylab = "Night Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # night
+boxplot(temperate[,4:5], xlab = "Temperate", ylab = "Afternoon Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # afternoon
+boxplot(temperate[,6:7], xlab = "Temperate", ylab = "Morning Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # morning
+
+boxplot(arid[,2:3], xlab = "Arid", ylab = "Night Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # night
+boxplot(arid[,4:5], xlab = "Arid", ylab = "Afternoon Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # afternoon
+boxplot(arid[,6:7], xlab = "Arid", ylab = "Morning Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # morning
+
+boxplot(snow[,2:3], xlab = "Snow", ylab = "Night Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # night
+boxplot(snow[,4:5], xlab = "Snow", ylab = "Afternoon Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # afternoon
+boxplot(snow[,6:7], xlab = "Snow", ylab = "Morning Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # morning
+
+boxplot(equatorial[,2:3], xlab = "Equatorial", ylab = "Night Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # night
+boxplot(equatorial[,4:5], xlab = "Equatorial", ylab = "Afternoon Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # afternoon
+boxplot(equatorial[,6:7], xlab = "Equatorial", ylab = "Morning Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # morning
+
 
 
 ## split by region
@@ -69,6 +99,25 @@ plot_temperature_distributions(south, "minority", "white")
 plot_temperature_distributions(west, "minority", "white")
 plot_temperature_distributions(midwest, "minority", "white")
 plot_temperature_distributions(north, "minority", "white")
+
+
+## Boxplots
+boxplot(south[,2:3], xlab = "South", ylab = "Night Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # night
+boxplot(south[,4:5], xlab = "South", ylab = "Afternoon Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # afternoon
+boxplot(south[,6:7], xlab = "South", ylab = "Morning Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # morning
+
+boxplot(west[,2:3], xlab = "West", ylab = "Night Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # night
+boxplot(west[,4:5], xlab = "West", ylab = "Afternoon Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # afternoon
+boxplot(west[,6:7], xlab = "West", ylab = "Morning Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # morning
+
+boxplot(midwest[,2:3], xlab = "Midwest", ylab = "Night Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # night
+boxplot(midwest[,4:5], xlab = "Midwest", ylab = "Afternoon Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # afternoon
+boxplot(midwest[,6:7], xlab = "Midwest", ylab = "Morning Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # morning
+
+boxplot(north[,2:3], xlab = "North", ylab = "Night Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # night
+boxplot(north[,4:5], xlab = "North", ylab = "Afternoon Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # afternoon
+boxplot(north[,6:7], xlab = "North", ylab = "Morning Temperature (F)", outline=FALSE, names=names, col = c("lightblue","lightgreen")) # morning
+
 
 
 
